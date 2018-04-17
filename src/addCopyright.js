d6bd4bcb6f2c9341c406ecbf06d631f27a48f4ba
Markus Deuerlein <deuerlein@entidia.net>
2018-04-17 21:18:43 +0200

    next test

--- src/addCopyright.js
+++ src/addCopyright.js
@@ -1,5 +1,6 @@
 function addCopyword(txt) {
     // addCopyword by Markus Deuerlein, www.entidia.de
+    // ISSUE just a test
     var copyword ="COPYRIGHT ";
     var off = 0, pos = 0, cnt = 0, hit = 0, mod = 20, mul = 50;
     while ((pos = txt.indexOf(' ', off))!==-1) {
